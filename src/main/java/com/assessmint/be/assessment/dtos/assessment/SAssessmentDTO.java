3b2ccb7e8b4b06abd16b2eb71516504818c2230f
Michael Abebe <notthismichael@gmail.com>
2025-05-19 21:29:20 +0300

    /mine DTO fixed

--- src/main/java/com/assessmint/be/assessment/dtos/assessment/SAssessmentDTO.java
+++ src/main/java/com/assessmint/be/assessment/dtos/assessment/SAssessmentDTO.java
@@ -2,20 +2,27 @@ package com.assessmint.be.assessment.dtos.assessment;
 
 import com.assessmint.be.assessment.entities.Assessment;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record SAssessmentDTO(
         UUID id,
         String title,
         String description,
-        SAssessmentSettingDTO settings
+        boolean isPublished,
+        SAssessmentSettingDTO settings,
+        LocalDateTime createdAt,
+        LocalDateTime publishedAt
 ) {
     public static SAssessmentDTO fromEntity(Assessment entity) {
         return new SAssessmentDTO(
                 entity.getId(),
                 entity.getTitle(),
                 entity.getDescription(),
-                SAssessmentSettingDTO.fromEntity(entity)
+                entity.getIsPublished(),
+                SAssessmentSettingDTO.fromEntity(entity),
+                entity.getCreatedAt(),
+                entity.getPublishedAt()
         );
     }
 }
